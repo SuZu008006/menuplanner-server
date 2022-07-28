1944441724676a6f6cffc77d913e6e9f78ffd633
Ryosuke Ito <suzusuke0806@gmail.com>
2022-07-29 08:30:22 +0900

    set frontend access

--- src/main/kotlin/com/menuplanner/server/Controller.kt
+++ src/main/kotlin/com/menuplanner/server/Controller.kt
@@ -1,11 +1,10 @@
 package com.menuplanner.server
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/greeting")
+@CrossOrigin(origins = ["http://localhost:3000", "https://menuplanner-web.herokuapp.com"])
 class Controller {
     @GetMapping
     fun getMessage(): List<String> {
